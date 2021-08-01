61bf26ff624539124c426bc2518f2e6f60f38744
Felix Schröter <fs@upsquared.com>
2021-08-01 13:28:31 +0200

    chore: remove unnecessary configuration

--- shell.nix
+++ shell.nix
@@ -5,22 +5,13 @@ in { pkgs ? import sources.nixpkgs { } }:
 with pkgs;
 with lib;
 
-let
-  dotnet-combined = with dotnetCorePackages; combinePackages [ sdk_5_0 ];
-  dotnetRoot = "${dotnet-combined}";
-  dotnetSdk = "${dotnet-combined}/sdk";
-  dotnetBinary = "${dotnetRoot}/bin/dotnet";
+let dotnet-combined = with dotnetCorePackages; combinePackages [ sdk_5_0 ];
 
 in mkShell {
   name = "env";
   buildInputs = [ niv dotnet-combined omnisharp-roslyn ];
 
   shellHook = ''
-    export DOTNET_ROOT=${escapeShellArg dotnetRoot}
-    local dotnetBase=${escapeShellArg dotnetSdk}/$(${
-      escapeShellArg dotnetBinary
-    } --version)
-    export MSBuildSdksPath=$dotnetBase/Sdks
-    export MSBUILD_EXE_PATH=$dotnetBase/MSBuild.dll
+    export DOTNET_ROOT=${escapeShellArg dotnet-combined}
   '';
 }
